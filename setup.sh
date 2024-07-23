3b555c12e9ee510846d2b8028c3c40c874a176ef
Ludovic Gardy <ludovic.gardy@gmail.com>
2024-07-23 22:20:46 +0200

    updated setup.sh  (removed theme)

--- setup.sh
+++ setup.sh
@@ -5,10 +5,3 @@ headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
-[theme]\n\
-base = \"dark\"\n\
-layout="centered"
-initial_sidebar_state="expanded"
-page_title="3D Med Viewer"
-sidebar_title="3D Med Viewer"
-" > ~/.streamlit/config.toml
