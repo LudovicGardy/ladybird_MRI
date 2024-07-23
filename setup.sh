717e85a0e1697df2c480b02595f18d44e371aaf8
Ludovic Gardy <ludovic.gardy@gmail.com>
2024-07-23 22:25:30 +0200

    updated setup.sh with syntax correction

--- setup.sh
+++ setup.sh
@@ -5,3 +5,10 @@ headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
+[theme]\n\
+base = \"dark\"\n\
+layout=\"centered\"\n\
+initial_sidebar_state=\"expanded\"\n\
+page_title=\"3D Med Viewer\"\n\
+sidebar_title=\"3D Med Viewer\"\n\
+" > ~/.streamlit/config.toml
